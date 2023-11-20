c900e23c544cc4e655cda864d371c62542a989c0
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-20 15:51:51 +0100

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -3,7 +3,27 @@ const { createApp } = Vue
 createApp({
 	data() {
 		return {
-
+			emailList : [],
 		}
+	},
+	methods: {
+
+		eMail() {
+			// genero 10 email
+			for(i = 0; i < 10; i++) {
+
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(r => {
+				console.log("Ricevuto: ", r.data);
+				let email = r.data.response;
+				this.emailList.push(email);
+				});
+			}
+		}
+			
+	},
+	mounted() {
+		console.log("Tutto ok!");
+
+		
 	}
 }).mount('#app')
